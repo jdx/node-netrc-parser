86deb7b5935eadc5de01803abbf7bf61ec2c1a68
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-03-01 18:37:40 -0800

    more work

--- src/netrc.js
+++ src/netrc.js
@@ -7,12 +7,11 @@ const path = require('path')
 /**
  * @typedef Machines
  * @type Object
- * @prop {string} host
+ * @prop {string} login
  * @prop {string} password
  */
 type Machine = {
   [prop: string]: string,
-  host: string,
   login?: string,
   password?: string
 }
@@ -22,43 +21,6 @@ type Machine = {
  * @type {Object.<string, Machine}
  */
 type Machines = {[name: string]: Machine}
-
-function lex (body: string): {token: string, line: number}[] {
-  const tokens = []
-  let lineIndex = 0
-  for (let line of body.split('\n')) {
-    line = line.trim()
-    if (line.includes(' ')) {
-      let idx = line.indexOf(' ')
-      tokens.push({token: line.slice(0, idx).trim(), line: lineIndex})
-      tokens.push({token: line.slice(idx).trim(), line: lineIndex})
-    } else {
-      tokens.push({token: line, line: lineIndex})
-    }
-    lineIndex++
-  }
-  return tokens
-}
-
-function parse (body: string): Machines {
-  const machines: Machines = {}
-  const tokens = lex(body)
-  let host
-  let prop
-  while (tokens.length > 0) {
-    prop = tokens.shift()
-    switch (prop.token) {
-      case 'machine':
-        host = tokens.shift().token
-        machines[host] = {host}
-        break
-      default:
-        if (!host) throw new Error(`Invalid token ${prop.token} on line ${prop.line}`)
-        machines[host][prop.token] = tokens.shift().token
-    }
-  }
-  return machines
-}
 
 /**
  * parses a netrc file
@@ -74,10 +36,66 @@ class Netrc {
   }
 
   constructor (file: string) {
-    this.machines = parse(fs.readFileSync(file, 'utf8'))
+    this._parse(file)
   }
 
   machines: Machines
+  default: Machine
+
+  _read (file: string): string {
+    if (path.extname(file) === '.gpg') return this._decrypt(file)
+    else return fs.readFileSync(file, 'utf8')
+  }
+
+  _decrypt (file: string): string {
+    const {spawnSync} = require('child_process')
+    const {stdout, status} = spawnSync('gpg', ['--batch', '--decrypt', file], {stdio: [0, null, 2], encoding: 'utf8'})
+    if (status !== 0) throw new Error(`gpg exited with code ${status}`)
+    return (stdout: any)
+  }
+
+  _lex (body: string): {token: string, line: number}[] {
+    const tokens = []
+    let lineIndex = 0
+    for (let line of body.split('\n')) {
+      line = line.split('#')[0].trim()
+      for (let token of line.split(' ')) {
+        token = token.trim()
+        if (!token) continue
+        tokens.push({token, line: lineIndex})
+      }
+      lineIndex++
+    }
+    return tokens
+  }
+
+  _parse (file: string) {
+    const body = this._read(file)
+    this.machines = {}
+    const tokens = this._lex(body)
+    let machine
+    let prop
+    while (tokens.length > 0) {
+      prop = tokens.shift()
+      switch (prop.token) {
+        case 'default':
+          this.default = machine = {}
+          break
+        case 'machine':
+          let host = tokens.shift().token
+          machine = this.machines[host] = {}
+          break
+        case 'macdef':
+          while (tokens.length > 0 && !['machine', 'default'].includes(tokens[0].token)) {
+            prop = tokens.shift()
+          }
+          break
+        default:
+          if (!machine) throw new Error(`Invalid token ${prop.token} on line ${prop.line} in ${file}`)
+          machine[prop.token] = tokens.shift().token
+      }
+    }
+  }
 }
 
 module.exports = Netrc
