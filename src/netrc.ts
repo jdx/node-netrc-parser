a9d6b135b7fd40019acbdb7699569c3b9af12ede
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-05 16:45:53 -0700

    simplified parser

--- src/netrc.ts
+++ src/netrc.ts
@@ -1,151 +1,133 @@
+import * as Execa from 'execa'
 import * as fs from 'fs'
 import * as os from 'os'
 import * as path from 'path'
-import * as Execa from 'execa'
 
-import Lex from './lex'
-import * as Token from './token'
+import {Machines, MachineToken, parse, Token} from './parse'
 
-let _debug: any
-function debug(...args: any[]) {
-  try {
-    if (process.env.NETRC_PARSER_DEBUG !== '1') return
-    if (!_debug) _debug = require('debug')('netrc-parser')
-    _debug(...args)
-  } catch (err) {}
-}
+const debug = require('debug')('netrc-parser')
 
-const stdio = [0, null, 2]
+export class Netrc {
+  file: string
+  machines!: Machines
 
-/**
- * parses a netrc file
- */
-export class Netrc extends Token.Base {
-  /**
-   * generates or parses a netrc file
-   * @example
-   * const {Netrc} = require('netrc-parser')
-   * const netrc = new Netrc()
-   * netrc.machines['api.heroku.com'].password // get auth token from ~/.netrc
-   */
   constructor(file?: string) {
-    super()
-    this._file = file
-  }
-
-  private _file: string | undefined
-  protected _tokens!: Token.Token[]
-  public get file() {
-    return this._file!
-  }
-  public get machines() {
-    return this._machines!
-  }
-  private _machines!: Token.Machines
-
-  get default(): Token.IMachine | undefined {
-    return this._tokens.find(t => t.type === 'default') as Token.DefaultMachine
-  }
-  set default(v: Token.IMachine | undefined) {
-    let idx = this._tokens.findIndex(t => t.type === 'default')
-    if (idx !== -1 && !v) this._tokens.splice(idx, 1)
-    else {
-      let newMachine = new Token.DefaultMachine({ ...v, post: '\n' })
-      if (idx !== -1 && v) this._tokens[idx] = newMachine
-      else if (v) this._tokens.push(newMachine)
-    }
+    this.file = file || this.defaultFile
   }
 
   async load() {
-    if (!this._file) this._file = await this.defaultFile()
-    this.parse(await this.readFile())
+    debug('load', this.file)
+    const decryptFile = async (): Promise<string> => {
+      const execa: typeof Execa = require('execa')
+      const {code, stdout} = await execa('gpg', this.gpgDecryptArgs, {stdio: [0, null, 2]})
+      if (code !== 0) throw new Error(`gpg exited with code ${code}`)
+      return stdout
+    }
+
+    let body = ''
+    if (path.extname(this.file) === '.gpg') {
+      body = await decryptFile()
+    } else {
+      body = await new Promise<string>((resolve, reject) => {
+        fs.readFile(this.file, {encoding: 'utf8'}, (err, data) => {
+          if (err && err.code !== 'ENOENT') reject(err)
+          resolve(data || '')
+        })
+      })
+    }
+    this.machines = parse(body)
   }
 
   loadSync() {
-    if (!this._file) this._file = this.defaultFile()
-    this.parse(this.readFileSync())
+    debug('loadSync', this.file)
+    const decryptFile = (): string => {
+      const execa: typeof Execa = require('execa')
+      const {stdout, status} = execa.sync('gpg', this.gpgDecryptArgs, {stdio: [0, null, 2]}) as any
+      if (status) throw new Error(`gpg exited with code ${status}`)
+      return stdout
+    }
+
+    let body = ''
+    if (path.extname(this.file) === '.gpg') {
+      body = decryptFile()
+    } else {
+      try {
+        body = fs.readFileSync(this.file, 'utf8')
+      } catch (err) {
+        if (err.code !== 'ENOENT') throw err
+      }
+    }
+
+    this.machines = parse(body)
   }
 
-  /**
-   * save the current home netrc with any changes
-   * @example
-   * const Netrc = require('netrc-parser')
-   * const netrc = new Netrc()
-   * await netrc.load()
-   * netrc.machines['api.heroku.com'].password = 'newpassword'
-   * netrc.save()
-   */
-  save() {
-    return this.write(this.content)
-  }
-
-  /**
-   * save the current home netrc with any changes
-   * @example
-   * const Netrc = require('netrc-parser')
-   * const netrc = new Netrc()
-   * netrc.loadSync()
-   * netrc.machines['api.heroku.com'].password = 'newpassword'
-   * netrc.saveSync()
-   */
-  saveSync() {
-    this.writeSync(this.content)
-  }
-
-  private get gpgEncryptArgs() {
-    const args = ['-a', '--batch', '--default-recipient-self', '-e']
-    debug('running gpg with args %o', args)
-    return args
-  }
-
-  private async write(body: string) {
+  async save() {
+    debug('save', this.file)
+    let body = this.output
     if (this.file.endsWith('.gpg')) {
       const execa: typeof Execa = require('execa')
-      const { stdout, code } = await execa('gpg', this.gpgEncryptArgs, { input: body, stdio: [null, null, 2] })
+      const {stdout, code} = await execa('gpg', this.gpgEncryptArgs, {input: body, stdio: [null, null, 2]})
       if (code) throw new Error(`gpg exited with code ${code}`)
       body = stdout
     }
     return new Promise((resolve, reject) => {
-      fs.writeFile(this.file, body, { mode: 0o600 }, err => (err ? reject(err) : resolve()))
+      fs.writeFile(this.file, body, {mode: 0o600}, err => (err ? reject(err) : resolve()))
     })
   }
 
-  private writeSync(body: string) {
+  saveSync() {
+    debug('saveSync', this.file)
+    let body = this.output
     if (this.file.endsWith('.gpg')) {
       const execa: typeof Execa = require('execa')
-      const { stdout, code } = execa.sync('gpg', this.gpgEncryptArgs, { input: body, stdio: [null, null, 2] }) as any
+      const {stdout, code} = execa.sync('gpg', this.gpgEncryptArgs, {input: body, stdio: [null, null, 2]}) as any
       if (code) throw new Error(`gpg exited with code ${status}`)
       body = stdout
     }
-    fs.writeFileSync(this.file, body, { mode: 0o600 })
+    fs.writeFileSync(this.file, body, {mode: 0o600})
   }
 
-  private parse(body: string) {
-    // prune the .netrc file if it is only whitespace
-    if (body.trim() === '') body = ''
-    let lex: typeof Lex = require('./lex').default
-    const tokens = lex(body)
-
-    let cur: Token.DefaultMachine | Token.Machine | this = this
-    for (let token of tokens) {
-      switch (token.type) {
-        case 'default':
-          cur = new Token.DefaultMachine(token)
-          this.addToken(cur)
-          break
-        case 'machine':
-          cur = new Token.Machine(token)
-          this.addToken(cur)
-          break
-        case 'newline':
-          cur = this
-          cur.addToken(token)
-          break
-        default:
-          cur.addToken(token)
+  private get output(): string {
+    let output: string[] = []
+    for (let t of this.machines._tokens as any as Token[]) {
+      if (t.type === 'other') {
+        output.push(t.content)
+        continue
+      }
+      if (t.pre) output.push(t.pre + '\n')
+      output.push(`machine ${t.host}`)
+      const addProps = (t: MachineToken) => {
+        const addProp = (k: string) => output.push(`${t.internalWhitespace}${k} ${t.props[k].value}${t.props[k].comment || ''}`)
+        // do login/password first
+        if (t.props.login) addProp('login')
+        if (t.props.password) addProp('password')
+        for (let k of Object.keys(t.props).filter(k => !['login', 'password'].includes(k))) {
+          addProp(k)
+        }
+      }
+      const addComment = (t: MachineToken) => t.comment && output.push(' ' + t.comment)
+      if (t.internalWhitespace.includes('\n')) {
+        addComment(t)
+        addProps(t)
+        output.push('\n')
+      } else {
+        addProps(t)
+        addComment(t)
+        output.push('\n')
       }
     }
-    this._machines = Token.machinesProxy(this._tokens)
+    return output.join('')
+  }
+
+  private get defaultFile(): string {
+    const home = (os.platform() === 'win32' &&
+        (process.env.HOME ||
+          (process.env.HOMEDRIVE && process.env.HOMEPATH && path.join(process.env.HOMEDRIVE!, process.env.HOMEPATH!)) ||
+          process.env.USERPROFILE)) ||
+      os.homedir() ||
+      os.tmpdir()
+    let file = path.join(home, os.platform() === 'win32' ? '_netrc' : '.netrc')
+    return fs.existsSync(file + '.gpg') ? (file += '.gpg') : file
   }
 
   private get gpgDecryptArgs() {
@@ -154,59 +136,11 @@ export class Netrc extends Token.Base {
     return args
   }
 
-  private async readFile(): Promise<string> {
-    const decryptFile = async (): Promise<string> => {
-      const execa: typeof Execa = require('execa')
-      const { code, stdout } = await execa('gpg', this.gpgDecryptArgs, { stdio })
-      if (code !== 0) throw new Error(`gpg exited with code ${code}`)
-      return stdout
-    }
-
-    if (path.extname(this.file) === '.gpg') return await decryptFile()
-    else {
-      return new Promise<string>((resolve, reject) => {
-        fs.readFile(this.file, { encoding: 'utf8' }, (err, data) => {
-          if (err && err.code !== 'ENOENT') reject(err)
-          resolve(data || '')
-        })
-      })
-    }
-  }
-
-  private readFileSync(): string {
-    const decryptFile = (): string => {
-      const execa: typeof Execa = require('execa')
-      const { stdout, status } = execa.sync('gpg', this.gpgDecryptArgs, { stdio }) as any
-      if (status) throw new Error(`gpg exited with code ${status}`)
-      return stdout
-    }
-
-    if (path.extname(this.file) === '.gpg') return decryptFile()
-    else {
-      try {
-        return fs.readFileSync(this.file, 'utf8')
-      } catch (err) {
-        if (err.code !== 'ENOENT') throw err
-        return ''
-      }
-    }
-  }
-
-  private get homedir() {
-    return (
-      (os.platform() === 'win32' &&
-        (process.env.HOME ||
-          (process.env.HOMEDRIVE && process.env.HOMEPATH && path.join(process.env.HOMEDRIVE!, process.env.HOMEPATH!)) ||
-          process.env.USERPROFILE)) ||
-      os.homedir() ||
-      os.tmpdir()
-    )
-  }
-
-  private defaultFile() {
-    let file = path.join(this.homedir, os.platform() === 'win32' ? '_netrc' : '.netrc')
-    return fs.existsSync(file + '.gpg') ? (file += '.gpg') : file
+  private get gpgEncryptArgs() {
+    const args = ['-a', '--batch', '--default-recipient-self', '-e']
+    debug('running gpg with args %o', args)
+    return args
   }
 }
 
-export default new Netrc()
+export default Netrc
---
+++ test/netrc.test.ts
@@ -0,0 +1,499 @@
+import {expect} from 'chai'
+import * as fs from 'fs-extra'
+
+import {Netrc} from '../src/netrc'
+
+fs.mkdirpSync('tmp')
+
+process.env.NETRC_PARSER_DEBUG = '1'
+
+const skipOnWindows = process.platform === 'win32' ? it.skip : it
+
+describe('netrc', () => {
+  it('can read system netrc', () => {
+    let netrc = new Netrc()
+    netrc.loadSync()
+    expect(!!netrc.machines).to.be.true
+  })
+
+  it('can read system netrc async', async () => {
+    let netrc = new Netrc()
+    await netrc.load()
+    expect(!!netrc.machines).to.be.true
+  })
+
+  it('reads basic', () => {
+    const f = 'tmp/netrc'
+    fs.writeFileSync(
+      f,
+      `machine mail.google.com # foo
+  login joe@gmail.com
+  account gmail
+  password somethingSecret
+machine ray login demo password mypassword`,
+    )
+    const netrc = new Netrc(f)
+    netrc.loadSync()
+
+    expect(netrc.machines['mail.google.com'].login).to.equal('joe@gmail.com')
+    expect(netrc.machines['mail.google.com'].account).to.equal('gmail')
+    expect(netrc.machines['mail.google.com'].password).to.equal('somethingSecret')
+
+    expect(netrc.machines.ray.login).to.equal('demo')
+    expect(netrc.machines.ray.password).to.equal('mypassword')
+  })
+
+  it('bad default order', () => {
+    const f = 'tmp/netrc'
+    fs.writeFileSync(
+      f,
+      `# I am a comment
+machine mail.google.com
+  login joe@gmail.com
+  account gmail
+  password somethingSecret
+# I am another comment
+
+default
+  login anonymous
+  password joe@example.com
+
+machine ray login demo password mypassword
+  `,
+    )
+    const netrc = new Netrc(f)
+    netrc.loadSync()
+
+    expect(netrc.machines['mail.google.com'].login).to.equal('joe@gmail.com')
+    expect(netrc.machines['mail.google.com'].account).to.equal('gmail')
+    expect(netrc.machines['mail.google.com'].password).to.equal('somethingSecret')
+
+    expect(netrc.machines.ray.login).to.equal('demo')
+    expect(netrc.machines.ray.password).to.equal('mypassword')
+  })
+
+  it('it loads the netrc file with comments', () => {
+    const f = 'tmp/netrc'
+    fs.writeFileSync(
+      f,
+      `machine api.dickeyxxx.com # foo
+  login jeff@foo.com
+  password myapikey`)
+    const netrc = new Netrc(f)
+    netrc.loadSync()
+
+    expect(netrc.machines['api.dickeyxxx.com'].login).to.equal('jeff@foo.com')
+    expect(netrc.machines['api.dickeyxxx.com'].password).to.equal('myapikey')
+  })
+
+  it('good', () => {
+    const f = 'tmp/netrc'
+    fs.writeFileSync(
+      f,
+      `# I am a comment
+machine mail.google.com
+\tlogin joe@gmail.com
+  account justagmail #end of line comment with trailing space
+  password somethingSecret
+ # I am another comment
+
+macdef allput
+put src/*
+
+macdef allput2
+  put src/*
+put src2/*
+
+machine ray login demo password mypassword
+
+machine weirdlogin login uname password pass#pass
+
+default
+  login anonymous
+  password joe@example.com
+`,
+    )
+    const netrc = new Netrc(f)
+    netrc.loadSync()
+
+    expect(netrc.machines['mail.google.com'].login).to.equal('joe@gmail.com')
+    expect(netrc.machines['mail.google.com'].account).to.equal('justagmail')
+    expect(netrc.machines['mail.google.com'].password).to.equal('somethingSecret')
+  })
+
+  const gpgEncrypted = `-----BEGIN PGP MESSAGE-----
+Version: GnuPG v2
+
+hIwD1rghrTHCzmIBA/9JIhd9NaY64C7QMIOa8KV/e97Hs9he6EAHdhDUMeb6/5HU
+KaxHX77rHjF0TxNUumQrMTfp+EjKzjuDqTxrv0TnpqB8JYhwLqVCGPM+OvjNlILy
+/EdDpkqEaKqM4KArRQjE4n8ifAi5CbldI/mO+oBvHTq5StJDNEhE+xMjRzGJ29LA
+VQEWWdR291Z8Y0cbZwX2DmGsPuo6tX0JeWQlG9ms8966wVk2LKFuUyynHBVjcsjv
+REKnai8ZixhaKRBE/NOiLo/Eqp6nI7/i8YU+mYV0rFljpLSnQ7LJcgw3ItyKXQ9F
+ws16ShzCIGM11JFySwb0NoV6H9VSakfu2LN1RpKFD2lvc6i75N0NWf0Jh/mKHFz+
+ugLe8sik/Zu8grrxtOVxfgtjFEQvjT3u02D4pDQP1lNp7SjVfqUC+XnxWQC+SQVC
+kKvydwB3oZqwHp6jpgLVTxjTfhm1vNTB7gAbgNOF63yQ/Wmrn3Pe38huh+TIKJCy
+pQgBLBordnqQajWt1ao+8AZiAsOooF0wJqm/mH1Og5/ADuhvZEQ=
+=PGaL
+-----END PGP MESSAGE-----`
+
+  skipOnWindows('good.gpg sync', () => {
+    const f = 'tmp/netrc.gpg'
+    fs.writeFileSync(f, gpgEncrypted)
+    const netrc = new Netrc(f)
+    netrc.loadSync()
+
+    expect(netrc.machines['mail.google.com'].login).to.equal('joe@gmail.com')
+    expect(netrc.machines['mail.google.com'].account).to.equal('justagmail')
+    expect(netrc.machines['mail.google.com'].password).to.equal('somethingSecret')
+
+    netrc.saveSync()
+    expect(fs.readFileSync(f, {encoding: 'utf8'})).to.contain('-----BEGIN PGP MESSAGE-----')
+  })
+
+  skipOnWindows('good.gpg', async () => {
+    const f = 'tmp/netrc.gpg'
+    await fs.writeFile(f, gpgEncrypted)
+    const netrc = new Netrc(f)
+    await netrc.load()
+
+    expect(netrc.machines['mail.google.com'].login).to.equal('joe@gmail.com')
+    expect(netrc.machines['mail.google.com'].account).to.equal('justagmail')
+    expect(netrc.machines['mail.google.com'].password).to.equal('somethingSecret')
+
+    await netrc.save()
+    expect(fs.readFileSync(f, {encoding: 'utf8'})).to.contain('-----BEGIN PGP MESSAGE-----')
+  })
+
+  it('saving', () => {
+    const f = 'tmp/netrc'
+    fs.writeFileSync(
+      f,
+      `# I am a comment
+machine mail.google.com
+\tlogin joe@gmail.com
+  password somethingSecret #end of line comment with trailing space
+ # I am another comment
+
+macdef allput
+put src/*
+
+macdef allput2
+  put src/*
+put src2/*
+
+machine ray login demo password mypassword
+
+machine weirdlogin login uname password pass#pass
+
+default
+  login anonymous
+  password joe@example.com
+`,
+  )
+    const netrc = new Netrc(f)
+    netrc.loadSync()
+    netrc.machines['mail.google.com'].login = 'joe2@gmail.com'
+    netrc.machines['mail.google.com'].account = 'justanaccount'
+    netrc.machines.ray.login = 'demo2'
+    netrc.machines.ray.account = 'newaccount'
+    netrc.machines.new = {login: 'myuser', password: 'mypass'}
+    netrc.machines.anothernew = {}
+    netrc.machines.anothernew = {login: 'myuser'}
+    netrc.saveSync()
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(`# I am a comment
+machine mail.google.com
+  login joe2@gmail.com
+  password somethingSecret #end of line comment with trailing space
+  account justanaccount
+ # I am another comment
+
+macdef allput
+put src/*
+
+macdef allput2
+  put src/*
+put src2/*
+
+machine ray login demo2 password mypassword account newaccount
+machine weirdlogin login uname password pass #pass
+
+default
+  login anonymous
+  password joe@example.com
+machine new
+  login myuser
+  password mypass
+machine anothernew
+  login myuser
+`)
+})
+
+  it('adding a machine should create a new entry', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = `machine api.dickeyxxx.com # foo
+  login jeff@foo.com
+  password myapikey`
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines['foo.bar.com'] = {login: 'foo@bar.com', password: 'foopassword'}
+    await netrc.save()
+
+    const afterSave = `machine api.dickeyxxx.com # foo
+  login jeff@foo.com
+  password myapikey
+machine foo.bar.com
+  login foo@bar.com
+  password foopassword\n`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('edit host', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = `machine a
+  login jeff@foo.com
+  password myapikey
+`
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines.a.host = 'b'
+    await netrc.save()
+
+    const afterSave = `machine b
+  login jeff@foo.com
+  password myapikey\n`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('edit existing machine', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = `machine api.dickeyxxx.com # foo
+  login jeff@foo.com
+  password myapikey
+machine b
+  login jeff@bar.com
+  password myapikey2`
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines['api.dickeyxxx.com'] = {login: 'foo@bar.com', password: 'foopassword'}
+    netrc.machines.b.login = 'foobar'
+    await netrc.save()
+
+    const afterSave = `machine api.dickeyxxx.com # foo
+  login foo@bar.com
+  password foopassword
+machine b
+  login foobar
+  password myapikey2\n`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('removing a machine', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = `machine api.dickeyxxx.com # foo
+  login jeff@foo.com
+  password myapikey
+machine foo.bar.com
+  password foopassword
+  login foo@bar.com
+`
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    delete netrc.machines['api.dickeyxxx.com']
+    await netrc.save()
+
+    const afterSave = `machine foo.bar.com
+  login foo@bar.com
+  password foopassword
+`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('setting machine to undefined', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = `machine api.dickeyxxx.com # foo
+  login jeff@foo.com
+  password myapikey
+machine foo.bar.com
+  password foopassword
+  login foo@bar.com
+`
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines['api.dickeyxxx.com'] = undefined as any
+    await netrc.save()
+
+    const afterSave = `machine foo.bar.com
+  login foo@bar.com
+  password foopassword
+`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('empty netrc', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = ''
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines['api.dickeyxxx.com'] = {login: 'foo', password: 'bar'}
+    netrc.machines['foo.dickeyxxx.com'] = {login: 'foo2', password: 'bar2'}
+    await netrc.save()
+
+    const afterSave = `machine api.dickeyxxx.com
+  login foo
+  password bar
+machine foo.dickeyxxx.com
+  login foo2
+  password bar2
+`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('netrc with nothing useful', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = 'foobar\n'
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines['api.dickeyxxx.com'] = {login: 'foo', password: 'bar'}
+    netrc.machines['foo.dickeyxxx.com'] = {login: 'foo2', password: 'bar2'}
+    await netrc.save()
+
+    const afterSave = `foobar
+machine api.dickeyxxx.com
+  login foo
+  password bar
+machine foo.dickeyxxx.com
+  login foo2
+  password bar2
+`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('netrc with duplicate entries', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = `machine a
+  login foo
+  password bar
+machine b
+  login foo2
+  password bar2
+machine a
+  login ignoreme
+  password ignoreme
+`
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    await netrc.save()
+
+    const afterSave = `machine a
+  login foo
+  password bar
+machine b
+  login foo2
+  password bar2
+`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+})
+
+  it('set existing', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = 'machine a password p login u'
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines.a = {login: 'foo', password: 'bar'}
+    await netrc.save()
+
+    const afterSave = 'machine a login foo password bar\n'
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+  })
+
+  it('set new prop', async () => {
+    const f = 'tmp/netrc'
+
+    const beforeSave = 'machine foo password p login u'
+
+    fs.writeFileSync(f, beforeSave)
+
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines.foo.login = 'uu'
+    netrc.machines.foo.account = 'bar'
+    netrc.machines.foo.password = undefined
+    expect('foo' in netrc.machines).to.equal(true)
+    expect('bar' in netrc.machines).to.equal(false)
+    delete netrc.machines.bar
+    await netrc.save()
+
+    const afterSave = `machine foo login uu account bar
+`
+
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+  })
+
+  it('file not found', async () => {
+    const f = 'tmp/netrc'
+    fs.removeSync(f)
+    const netrc = new Netrc(f)
+    await netrc.load()
+    netrc.machines.foo = {login: 'u', password: 'p'}
+    await netrc.save()
+    const afterSave = 'machine foo\n  login u\n  password p\n'
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+  })
+
+  it('file not found sync', () => {
+    const f = 'tmp/netrc'
+    fs.removeSync(f)
+    const netrc = new Netrc(f)
+    netrc.loadSync()
+    netrc.machines.foo = {login: 'u', password: 'p'}
+    netrc.saveSync()
+    const afterSave = 'machine foo\n  login u\n  password p\n'
+    expect(fs.readFileSync(f, 'utf8')).to.equal(afterSave)
+  })
+})
