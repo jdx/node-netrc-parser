126c25c8107ae8c508040cf0b5bf5f59f41aab30
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-09 13:14:59 -0700

    fix: make machines iterable and add debug output

--- src/netrc.ts
+++ src/netrc.ts
@@ -3,10 +3,54 @@ import * as fs from 'fs'
 import * as os from 'os'
 import * as path from 'path'
 
-import {Machines, MachineToken, parse, Token} from './parse'
-
 const debug = require('debug')('netrc-parser')
 
+export function parse(body: string): Machines {
+  const lines = body.split('\n')
+  let pre: string[] = []
+  let machines: MachineToken[] = []
+  while (lines.length) {
+    const line = lines.shift()!
+    const match = line.match(/machine\s+((?:[^#\s]+[\s]*)+)(#.*)?$/)
+    if (!match) {
+      pre.push(line)
+      continue
+    }
+    const [, body, comment] = match
+    const machine: MachineToken = {
+      type: 'machine',
+      host: body.split(' ')[0],
+      pre: pre.join('\n'),
+      internalWhitespace: '\n  ',
+      props: {},
+      comment,
+    }
+    pre = []
+    // do not read other machines with same host
+    if (!machines.find(m => m.type === 'machine' && m.host === machine.host)) machines.push(machine)
+    if (body.trim().includes(' ')) { // inline machine
+      const [host, ...propStrings] = body.split(' ')
+      for (let a = 0; a < propStrings.length; a += 2) {
+        machine.props[propStrings[a]] = {value: propStrings[a + 1]}
+      }
+      machine.host = host
+      machine.internalWhitespace = ' '
+    } else { // multiline machine
+      while (lines.length) {
+        const line = lines.shift()!
+        const match = line.match(/^(\s+)([\S]+)\s+([\S]+)(\s+#.*)?$/)
+        if (!match) {
+          lines.unshift(line)
+          break
+        }
+        const [, ws, key, value, comment] = match
+        machine.props[key] = {value, comment}
+        machine.internalWhitespace = `\n${ws}`
+      }
+    }
+  }
+  return proxify([...machines, {type: 'other', content: pre.join('\n')}])
+}
 export class Netrc {
   file: string
   machines!: Machines
@@ -31,11 +75,13 @@ export class Netrc {
       body = await new Promise<string>((resolve, reject) => {
         fs.readFile(this.file, {encoding: 'utf8'}, (err, data) => {
           if (err && err.code !== 'ENOENT') reject(err)
+          debug('ENOENT')
           resolve(data || '')
         })
       })
     }
     this.machines = parse(body)
+    debug('machines: %o', Object.keys(this.machines))
   }
 
   loadSync() {
@@ -59,6 +105,7 @@ export class Netrc {
     }
 
     this.machines = parse(body)
+    debug('machines: %o', Object.keys(this.machines))
   }
 
   async save() {
@@ -144,3 +191,90 @@ export class Netrc {
 }
 
 export default new Netrc()
+
+export type Token = MachineToken | {type: 'other', content: string}
+export type MachineToken = {
+  type: 'machine'
+  pre?: string
+  host: string
+  internalWhitespace: string
+  props: {[key: string]: {value: string, comment?: string}}
+  comment?: string
+}
+
+export type Machines = {
+  [key: string]: {
+    login?: string
+    password?: string
+    account?: string
+    [key: string]: string | undefined
+  }
+}
+
+// this is somewhat complicated but it takes the array of parsed tokens from parse()
+// and it creates ES6 proxy objects to allow them to be easily modified by the consumer of this library
+function proxify(tokens: Token[]): Machines {
+  const proxifyProps = (t: MachineToken) => new Proxy(t.props as any as {[key: string]: string}, {
+    get(_, key: string) {
+      if (key === 'host') return t.host
+      // tslint:disable-next-line strict-type-predicates
+      if (typeof key !== 'string') return t.props[key]
+      const prop = t.props[key]
+      if (!prop) return
+      return prop.value
+    },
+    set(_, key: string, value: string) {
+      if (key === 'host') {
+        t.host = value
+      } else if (!value) {
+        delete t.props[key]
+      } else {
+        t.props[key] = t.props[key] || (t.props[key] = {value: ''})
+        t.props[key].value = value
+      }
+      return true
+    },
+  })
+  const machineTokens = tokens.filter((m): m is MachineToken => m.type === 'machine')
+  const machines = machineTokens.map(proxifyProps)
+  const getWhitespace = () => {
+    if (!machineTokens.length) return ' '
+    return machineTokens[machineTokens.length - 1].internalWhitespace
+  }
+  const obj: Machines = {}
+  obj._tokens = tokens as any
+  for (let m of machines) obj[m.host] = m
+  return new Proxy(obj, {
+    set(obj, host: string, props: {[key: string]: string}) {
+      if (!props) {
+        delete obj[host]
+        const idx = tokens.findIndex(m => m.type === 'machine' && m.host === host)
+        if (idx === -1) return true
+        tokens.splice(idx, 1)
+        return true
+      }
+      let machine = machines.find(m => m.host === host)
+      if (!machine) {
+        const token: MachineToken = {type: 'machine', host, internalWhitespace: getWhitespace(), props: {}}
+        tokens.push(token)
+        machine = proxifyProps(token)
+        machines.push(machine)
+        obj[host] = machine
+      }
+      for (let [k, v] of Object.entries(props)) {
+        machine[k] = v
+      }
+      return true
+    },
+    deleteProperty(obj, host: string) {
+      delete obj[host]
+      const idx = tokens.findIndex(m => m.type === 'machine' && m.host === host)
+      if (idx === -1) return true
+      tokens.splice(idx, 1)
+      return true
+    },
+    ownKeys() {
+      return machines.map(m => m.host)
+    },
+  })
+}
